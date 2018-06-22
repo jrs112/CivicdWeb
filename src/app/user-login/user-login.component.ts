87b6e3ba137a5148d166727c537f8e8a358bbfe0
jrs112 <jrjackets112@gmail.com>
2018-06-21 23:13:03 -0400

    add front end to login page

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -16,4 +16,32 @@ export class UserLoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  logUserIn() {
+  // this.userApiService.logInUser()
+  // .subscribe(
+  //   (data: any) => {
+  //     console.log(data);
+  //     this.tokenManagerService.createToken(data);
+  //     this.navCtrl.push(HomePage);
+  //     //***** THIS IS AUTH CODE TO PUT IN WHEN THE USER CONTROLLER IS SET UP IN THE API
+  //   //   this.userApiService.getUserInfo(data.userName)
+  //   //   .subscribe(
+  //   //     (userInfo: any) => {
+  //   //       this.userApiService.setUserInfo(data);
+  //   //     },
+  //   //     (error) => {
+  //   //       console.log("something went wrong: ", error)
+  //   //     }
+  //   //   )
+  //     // this.navCtrl.push(HomePage);
+  //   // },
+  //   // (err) => {
+  //   //   console.log("got an error: ", err);
+  //   //   this.loginErrMsg = err.error.error_description;
+  //   //
+  //   // }
+  // }
+  // )
+}
+
 }
